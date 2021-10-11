422c5ba25d5d5b259194ecc0d77477501f9682bd
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-11 23:00:13 +0530

    Initial Setup Updated v2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Cart</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
