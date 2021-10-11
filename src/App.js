1eeed64f91cc7e68abb4ba61ad4783c1c572ac6b
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-11 23:15:54 +0530

    Created Cart Item

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
+import CartItem from "./CartItem";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Cart</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CartItem/>
       </header>
     </div>
   );
