5aff220b50c3f6452a2fb64b61fc0eb69c9875e0
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-13 22:21:15 +0530

    Integrated firebase with whole project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Cart from './Cart'
 import Navbar from './Navbar'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 
 class App extends React.Component {
     constructor() {
@@ -8,41 +10,56 @@ class App extends React.Component {
         //it is constructor for the method from which current class is extended
         super()
         this.state = {
-            products: [
-                {
-                    price: 25999,
-                    basePrice: 25999,
-                    title: 'Samsung M52 5G',
-                    qty: 1,
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShYzbEXLNrRpw0qf3HjHT8MuuGpei9Fz8pYw&usqp=CAU',
-                    id: 1,
-                },
-                {
-                    basePrice: 78999,
-                    price: 78999,
-                    title: 'Lenovo Legion 5',
-                    qty: 1,
-                    img: 'https://media.croma.com/image/upload/v1623994146/Croma%20Assets/Computers%20Peripherals/Laptop/Images/235451_qy3dvi.png',
-                    id: 2,
-                },
-                {
-                    price: 9999,
-                    basePrice: 9999,
-                    title: 'Sonata Classic Watch',
-                    qty: 1,
-                    img: 'https://staticimg.titan.co.in/Sonata/Catalog/87029NL06_1.jpg',
-                    id: 3,
-                },
-                {
-                    price: 25999,
-                    basePrice: 25999,
-                    title: 'Samsung 250GB NVME M2 SSD',
-                    qty: 1,
-                    img: 'https://deltapage.com/image/cache/catalog/New_images/Samsung/samsung-980-pro-500gb_2-550x550h.jpg',
-                    id: 4,
-                },
-            ],
+            products: [],
+            isLoading: true,
         }
+    }
+
+    componentDidMount() {
+        // let products = []
+        // firebase
+        //     .firestore()
+        //     .collection('products')
+        //     .get()
+        //     .then((snapshot) => {
+        //         snapshot.docs.map((doc) => {
+        //             console.log(doc.data())
+        //             // products.push(doc.data())
+        //         })
+        //         const products = snapshot.docs.map((doc) => {
+        //             const data = doc.data()
+        //             data['id'] = doc.id
+        //             return data
+        //         })
+        //         // console.log('products ', products)
+
+        //         this.setState({
+        //             products: products,
+        //             isLoading: false,
+        //         })
+        //     })
+
+        //sync with firebase continuously with onSnapshot function
+        firebase
+            .firestore()
+            .collection('products')
+            .onSnapshot((snapshot) => {
+                snapshot.docs.map((doc) => {
+                    console.log(doc.data())
+                    // products.push(doc.data())
+                })
+                const products = snapshot.docs.map((doc) => {
+                    const data = doc.data()
+                    data['id'] = doc.id
+                    return data
+                })
+                // console.log('products ', products)
+
+                this.setState({
+                    products: products,
+                    isLoading: false,
+                })
+            })
     }
 
     handleIncrease = (product) => {
@@ -51,12 +68,26 @@ class App extends React.Component {
         const { products } = this.state
         //now find the index of product whose qty we need to increase
         const index = products.indexOf(product)
-        products[index].qty = products[index].qty + 1
-        products[index].price = products[index].qty * products[index].basePrice
-        this.setState({
-            products: products,
-            // we can simply put 'products' only as key and value both are same
-        })
+        // products[index].qty = products[index].qty + 1
+        // products[index].price = products[index].qty * products[index].basePrice
+        // this.setState({
+        //     products: products,
+        //     // we can simply put 'products' only as key and value both are same
+        // })
+        const docRef = firebase
+            .firestore()
+            .collection('products')
+            .doc(products[index].id)
+        docRef
+            .update({
+                qty: products[index].qty + 1,
+            })
+            .then(() => {
+                console.log('Document updated !')
+            })
+            .catch((err) => {
+                console.log('Error :: ', err)
+            })
     }
 
     handleDecrease = (product) => {
@@ -69,13 +100,20 @@ class App extends React.Component {
         if (products[index].qty === 0) {
             return
         }
-        console.log('Hey decrease product qty : ', product)
-        products[index].qty = products[index].qty - 1
-        products[index].price = products[index].qty * products[index].basePrice
-        this.setState({
-            products: products,
-            // we can simply put 'products' only as key and value both are same
-        })
+        const docRef = firebase
+            .firestore()
+            .collection('products')
+            .doc(products[index].id)
+        docRef
+            .update({
+                qty: products[index].qty - 1,
+            })
+            .then(() => {
+                console.log('Document deleted !')
+            })
+            .catch((err) => {
+                console.log('Error :: ', err)
+            })
     }
 
     handleDelete = (product) => {
@@ -85,10 +123,23 @@ class App extends React.Component {
         //now find the index of product whose qty we need to delete
         const index = products.indexOf(product)
         //delete the product from array
-        products.splice(index, 1)
-        this.setState({
-            products: products,
-        })
+        // products.splice(index, 1)
+        // this.setState({
+        //     products: products,
+        // })
+
+        const docRef = firebase
+            .firestore()
+            .collection('products')
+            .doc(products[index].id)
+        docRef
+            .delete()
+            .then(() => {
+                console.log('Document uploaded !')
+            })
+            .catch((err) => {
+                console.log('Error :: ', err)
+            })
     }
 
     getCartCount = () => {
@@ -104,17 +155,37 @@ class App extends React.Component {
         const { products } = this.state
         let famount = 0
         products.forEach((product) => {
-            famount = famount + product.price
+            famount = famount + product.qty * product.price
         })
         return famount
     }
 
+    addProduct = () => {
+        firebase
+            .firestore()
+            .collection('products')
+            .add({
+                img: '',
+                price: 9999,
+                qty: 3,
+                title: 'Washing machine',
+            })
+            .then((docRef) => {
+                console.log(docRef, ' product has been added.')
+            })
+            .catch((err) => {
+                console.log(err, ' error in adding project.')
+            })
+    }
+
     render() {
-        const { products } = this.state
+        const { products, isLoading } = this.state
         return (
             <div className="App">
+                {isLoading && <h1>Loading products...</h1>}
                 {/* to show the count of items on cart */}
                 <Navbar getCount={this.getCartCount()} />
+                {/* <button onClick={this.addProduct}>Add a product</button> */}
                 <header className="App-header">
                     <h1>Cart</h1>
                     <Cart
--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyAP9aiyjcpaSILgNXbh61UUdidvpGhgvWQ',
+    authDomain: 'ec-cart.firebaseapp.com',
+    projectId: 'ec-cart',
+    storageBucket: 'ec-cart.appspot.com',
+    messagingSenderId: '742600826515',
+    appId: '1:742600826515:web:bd37ff7f3713d7168a3766',
+}
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig)
+
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
